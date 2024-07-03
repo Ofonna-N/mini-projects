c1924cad2d52a9acdc42522689bc711443e74f68
Ofonna-N <nwezeofonna@gmail.com>
2024-07-03 03:33:41 -0500

    Add project card view code link and project cards dynamic rendering

--- src/projects/home_page/scripts/home.ts
+++ src/projects/home_page/scripts/home.ts
@@ -1,12 +1,13 @@
 const heroOverViewText = document.getElementById("hero-block__description");
+const projectsContainer = document.getElementById("projects-block__grid");
 
 type Project = {
   title: string;
   description: string;
   techStack: string[];
-  demoLink: string;
   codeLink?: string;
   thumbnail?: string;
+  thumbnailAlt?: string;
 };
 
 const projectsData: Project[] = [
@@ -15,9 +16,9 @@ const projectsData: Project[] = [
     description:
       "A simple quote generator that fetches random quotes from an API.",
     techStack: ["HTML", "CSS", "JavaScript"],
-    demoLink: "quote-generator/index.html",
-    thumbnail: "quote-generator/thumbnail.png",
-    codeLink: "",
+    thumbnail: "/src/assets/sample.jpg",
+    thumbnailAlt: "Quote Generator",
+    codeLink: "https://developer.mozilla.org/en-US/docs/Web/API/Location",
   },
 ];
 
@@ -42,8 +43,49 @@ const updateHeroOverviewText = () => {
   }
 };
 
+const getProjectCard = (project: Project) => {
+  return `<div class="project-card">
+                <a href="/projects/01-rotating-navigation">
+                  <img
+                    src=${project.thumbnail}
+                    alt=${project.thumbnailAlt}
+                    class="project-card__image"
+                  />
+                  <div class="project-card__content">
+                             <div class="project-card__title-container">
+                      <h3 class="project-card__title">${project.title}</h3>
+
+                      <i
+                        class="fa-brands fa-github"
+                        style="color: #ffffff"
+                        onclick='window.open("${project.codeLink}", "_blank")'
+                      ></i>
+                    </div>
+                    <p class="project-card__desc">
+                      ${project.description}
+                    </p>
+                    <div class="project-card__tech-stack">
+                      ${project.techStack
+                        .map(
+                          (stack) =>
+                            `<span class="project-card__tag">${stack}</span>`
+                        )
+                        .join(" ")}
+                    </div>
+                  </div>
+                </a>
+              </div>`;
+};
+
+const populateProjects = () => {
+  projectsContainer!.innerHTML = projectsData
+    .map((project) => getProjectCard(project))
+    .join("");
+};
+
 const initHome = () => {
   updateHeroOverviewText();
+  populateProjects();
   window.addEventListener("resize", updateHeroOverviewText);
 };
 
